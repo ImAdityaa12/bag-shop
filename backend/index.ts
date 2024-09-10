ce7bea8ac94cd4d10a5665d979d340370c537be8
ImAdityaa12 <adityagupta1291@gmail.com>
2024-09-10 23:38:07 +0530

    Backend and Frontend Setup

---
+++ backend/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+// mongoose
+//   .connect(process.env.MONGODB_CONNECTION_STRING as string)
+//   .then(() => console.log("Connected to database!"));
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(7000, () => {
+  console.log("Server started on localhost 7000");
+});
