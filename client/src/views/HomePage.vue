4830fff59c368b2c296006e98565525bc821652a
ImAdityaa12 <adityagupta1291@gmail.com>
2024-09-29 02:41:40 +0530

    Added Homepage component

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
-// import { Button } from "../components/ui/button";
 import { Button } from "@/components/ui/button";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+const getCookie = (name: string): string | undefined => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift();
+  return undefined;
+};
+
+const cookieValue = ref<string | null>(null);
+const router = useRouter();
+onMounted(() => {
+  const token = getCookie("token");
+  cookieValue.value = token ?? null;
+  if (!cookieValue.value) {
+    router.push("/create");
+  }
+});
 </script>
 
 <template>
-  <Button> Hello </Button>
+  <Button>Hello</Button>
 </template>
 
 <style scoped>
