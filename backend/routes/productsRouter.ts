9340abce33c6902b5523e8df0d45120a60774f35
ImAdityaa12 <adityagupta1291@gmail.com>
2024-09-30 12:00:41 +0530

    Added Create Product Route

--- backend/routes/productsRouter.ts
+++ backend/routes/productsRouter.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import upload from "../config/multer-config";
+import productModel from "../models/productModel";
 
 const router = express.Router();
-router.get("/", function (req, res) {
-  res.send("Hello World!");
+router.post("/create", upload.single("file"), async function (req, res) {
+  const { name, price, discount, bgColor, panelColor, textColor } = req.body;
+  await productModel.create({
+    image: req.file?.buffer,
+    name,
+    price,
+    discount,
+    bgColor,
+    panelColor,
+    textColor,
+  });
+  return res.status(201).send("Product created successfully");
 });
 export default router;
