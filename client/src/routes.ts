ffcf92cac8e47787a13c7e69e1172ac49a4131a9
ImAdityaa12 <adityagupta1291@gmail.com>
2024-10-02 15:17:57 +0530

    Updated Routes

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -3,6 +3,9 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import CreateAccountPage from "./views/CreateAccountPage.vue";
 import HomePage from "./views/HomePage.vue";
 import CreateProduct from "./views/CreateProduct.vue";
+import Shop from "./views/Shop.vue";
+import AboutUs from "./views/AboutUs.vue";
+import ContactUs from "./views/ContactUs.vue";
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
@@ -17,6 +20,18 @@ const router = createRouter({
     {
       path: "/create-product",
       component: CreateProduct,
+    },
+    {
+      path: "/shop",
+      component: Shop,
+    },
+    {
+      path: "/about",
+      component: AboutUs,
+    },
+    {
+      path: "/contact",
+      component: ContactUs,
     },
   ],
 });
