cc905cf92f037df50c2f5eab4da7d912e2ca6ca5
ImAdityaa12 <adityagupta1291@gmail.com>
2024-10-02 15:21:09 +0530

    Added Get Products API

--- backend/routes/productsRouter.ts
+++ backend/routes/productsRouter.ts
@@ -3,17 +3,34 @@ import upload from "../config/multer-config";
 import productModel from "../models/productModel";
 
 const router = express.Router();
+router.get("/", async function (req, res) {
+  try {
+    const products = await productModel.find();
+    if (!products) {
+      return res.status(404).send("No products found");
+    }
+    return res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("An error occurred while fetching products");
+  }
+});
 router.post("/create", upload.single("file"), async function (req, res) {
-  const { name, price, discount, bgColor, panelColor, textColor } = req.body;
-  await productModel.create({
-    image: req.file?.buffer,
-    name,
-    price,
-    discount,
-    bgColor,
-    panelColor,
-    textColor,
-  });
-  return res.status(201).json("Product created successfully");
+  try {
+    const { name, price, discount, bgColor, panelColor, textColor } = req.body;
+    await productModel.create({
+      image: req.file?.buffer,
+      name,
+      price,
+      discount,
+      bgColor,
+      panelColor,
+      textColor,
+    });
+    return res.status(201).json("Product created successfully");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("An error occurred while creating the product");
+  }
 });
 export default router;
